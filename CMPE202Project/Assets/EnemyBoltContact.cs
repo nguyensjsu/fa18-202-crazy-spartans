b1cdb14797e2a3b0ce5a4fd152d26ba9679bb586
ShikangJin <shikang.jin@sjsu.edu>
2018-11-29 13:35:49 -0800

    space shooter

---
+++ CMPE202Project/Assets/EnemyBoltContact.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBoltContact : MonoBehaviour
+{
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int score;
+    private GameController gameController;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag != "Player")
+        {
+            return;
+        }
+
+
+        Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        gameController.GameOver();
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        gameController.addScore(score);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+    // Use this for initialization
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameControllerObject == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ CMPE202Project/Assets/HazardTwoFactory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class HazardTwoFactory : HazardFactory
+{
+    public static GameObject hazard;
+
+    public void setHazard(GameObject inputHazard)
+    {
+        hazard = inputHazard;
+    }
+
+
+    public GameObject createHazard()
+   
+    {
+        return hazard;
+    }
+
+}
